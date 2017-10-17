194e2c762e9de3faa315f7fc660026daf3bd3896
EstelleEvelyn <estelle.bayer@gmail.com>
2017-10-17 13:42:15 -0500

    er

--- lounge.c
+++ lounge.c
@@ -1,5 +1,12 @@
+/* Estelle Bayer
+CS 332 Take Home Exam
+
+A thread-based solution to making professors and deans share space.
+*/
+
 #include "loungeTest.h"
 
+//to avoid typos and confusion
 #define EMPTY 0
 #define MATH 1
 #define CS 2
@@ -13,63 +20,83 @@ int waitingDeans;
 void init() {
   pthread_mutex_init(&lock, NULL);
   pthread_cond_init(&enterLounge, NULL);
+  //how many people are in the lounge
   occupancy = 0;
+  //how many deans are at the door
   waitingDeans = 0;
+  //what does the sign say
   sign = 0;
   return;
 }
 
 void mathProfArrive() {
   pthread_mutex_lock(&lock);
+  //math prof must wait while there are CS profs or deans in the lounge,
+  //or if there are deans waiting at the door with them
   while((sign != MATH && sign != EMPTY) || waitingDeans != 0) {
     pthread_cond_wait(&enterLounge, &lock);
   }
+  //first prof in should change sign
   if (sign == EMPTY) {
     sign = MATH;
   }
+  //update occupancy from inside the lounge
   occupancy++;
-  pthread_cond_signal(&enterLounge);
+  //signal so other math profs can enter
+  pthread_cond_broadcast(&enterLounge);
   pthread_mutex_unlock(&lock);
   return;
 }
 
 void csProfArrive() {
   pthread_mutex_lock(&lock);
+  //math prof waits while math profs or deans occupy lounge
+  //or if deans are visible waiting at door
   while((sign != CS && sign != EMPTY) || waitingDeans != 0) {
     pthread_cond_wait(&enterLounge, &lock);
   }
+  //first prof in changes sign
   if (sign == EMPTY) {
     sign = CS;
   }
+  //update occupancy in lounge
   occupancy++;
-  pthread_cond_signal(&enterLounge);
+  //signal to let other CS profs enter
+  pthread_cond_broadcast(&enterLounge);
   pthread_mutex_unlock(&lock);
   return;
 }
 
 void deanArrive() {
   pthread_mutex_lock(&lock);
+  //there are deans milling about outside
   waitingDeans++;
-  // pthread_mutex_unlock(&lock);
+  //wait to enter while there are profs in the room
   while(sign != DEAN && sign != EMPTY) {
     pthread_cond_wait(&enterLounge, &lock);
   }
+  //first dean in changes sign
   if (sign == EMPTY) {
     sign = DEAN;
   }
+  //there is one more person in the lounge and one fewer dean milling outside
   occupancy++;
   waitingDeans--;
-  pthread_cond_signal(&enterLounge);
+  //let other deans enter
+  pthread_cond_broadcast(&enterLounge);
   pthread_mutex_unlock(&lock);
   return;
 }
 
 void mathProfLeave() {
   pthread_mutex_lock(&lock);
+  //there's one fewer person in the lounge
   occupancy--;
+  //last one out indicates that lounge is empty
   if(occupancy == 0) {
     sign = EMPTY;
   }
+  //let other people use the lounge
   pthread_cond_broadcast(&enterLounge);
   pthread_mutex_unlock(&lock);
   return;
@@ -77,10 +104,13 @@ void mathProfLeave() {
 
 void csProfLeave() {
   pthread_mutex_lock(&lock);
+  //one fewer person in lounge
   occupancy--;
+  //last out changes sign to empty
   if(occupancy == 0) {
     sign = EMPTY;
   }
+  //let others use lounge
   pthread_cond_broadcast(&enterLounge);
   pthread_mutex_unlock(&lock);
   return;
@@ -88,10 +118,13 @@ void csProfLeave() {
 
 void deanLeave() {
   pthread_mutex_lock(&lock);
+  //one fewer dean
   occupancy--;
+  //last out changes sign
   if(occupancy == 0) {
     sign = EMPTY;
   }
+  //let others in
   pthread_cond_broadcast(&enterLounge);
   pthread_mutex_unlock(&lock);
   return;
